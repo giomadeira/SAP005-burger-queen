78f408baf98e8bf40869ef9116abf35612626836
giomadeira <giomadeira@gmail.com>
2021-02-19 12:59:36 -0300

    adicionado os tipos de hamburguer ao cardapio

--- src/components/Cardapio/index.js
+++ src/components/Cardapio/index.js
@@ -13,9 +13,9 @@ function Cardapio({className, title, array}){
               array && array.map((item) => (
                   <div className="container-cardapio">
                   <h1 className="divName">{item.name}</h1>
+                  <h1 className="divName">{item.flavor}</h1>
                   <h1 className="divPrice">R${item.price},00</h1>
-                 </div>
-                  
+                 </div>      
               ))
           }
           </div>   
--- src/pages/Hall/index.js
+++ src/pages/Hall/index.js
@@ -10,8 +10,15 @@ function Hall(){
         history.push('/')
     }
 
+    const logout = () => {
+        const token  = localStorage.getItem("token");
+        localStorage.clear()
+        routerBack()
+    }
+
     const [cafe, setCafe] = useState('');
     const [menu, setMenu] = useState('');
+    
     const token  = localStorage.getItem("token");
 
     useEffect (() => {
@@ -26,25 +33,61 @@ function Hall(){
             .then((response) => response.json()).then((json) => {
                 const breakfast = json.filter(item => item.type === 'breakfast')
                 const allDay = json.filter(item => item.type === 'all-day')
-                setCafe(breakfast)
-                setMenu(allDay)
-                console.log(json);
+                const hamburgerMap = {}
+                allDay.map((item, i , self) => {
+
+                    if (!hamburgerMap[item.name]){
+                        hamburgerMap[item.name] = []
+                    }
+
+                    if (hamburgerMap[item.name].indexOf(item.flavor)== -1){
+                        hamburgerMap[item.name].push(item.flavor)
+                        }    
+                } )
+                console.log(hamburgerMap);
+                 const hamburgerSelect = 
+                            {
+                            'Hambug Simples': [
+                            'carne', 'frango', 'vegetariano'
+                            ],
+                            'Hambug Duplo':  [
+                            'carne', 'frango', 'vegetariano'
+                            ],
+                        }
+  
+  
+    console.log(Object.keys(hamburgerSelect))
+  
+    Object.keys(hamburgerSelect).map((item, i) => {
+  	console.log(hamburgerSelect[item])
+    hamburgerSelect[item].map((flavors) =>{
+    	console.log(flavors)
+    })
+  })
+                
+                setCafe(breakfast);
+                setMenu(allDay);
             })
     }, []);
     
+  
     
     return(
         <div className="App">
-             <button className="btnExit" onClick={ routerBack}>Sair</button>
+             <button className="btnExit" onClick={logout}>Logout</button>
+
             <div className="cafe">
                 <h1>Café da Manhã</h1>
             <Cardapio className="container-cafe" title="" array={cafe} />
             </div>
+           
 
             <div className="allDay">
                 <h1>Almoço e Jantar</h1>
             <Cardapio className="container-allDay" title="" array={menu} />
             </div>
+
+            
 
         </div>    
     );
